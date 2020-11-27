ce0c882083efc99948b01e212945a3e8727fee12
Subhra Sarkar <rurtle.coder@gmail.com>
2020-11-27 21:00:26 +0530

    Modified source for adding keyboard support

--- task_05/rurtles_hello.c
+++ task_05/rurtles_hello.c
@@ -2,16 +2,16 @@
 // Author: Subhra Sankha Sarkar <rurtle.coder@gmail.com>
 
 /*
- * @brief	Task 05 - Write a Linux kernel module
+ * @brief	Task 05 - Write a Linux kernel module which gets loaded when a
+ * 		wireless USB keyboard is plugged in
  */
 #include <linux/init.h>
 #include <linux/module.h>				/* Needed by all modules */
 #include <linux/kernel.h>
 #include <linux/usb.h>
-#include <linux/usb/storage.h>
+#include <linux/hid.h>
 
 
-#if 0
 static const struct usb_device_id usb_kbd_id_table[] = {
 	{ USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
 			USB_INTERFACE_SUBCLASS_BOOT,
@@ -19,36 +19,9 @@ static const struct usb_device_id usb_kbd_id_table[] = {
 	},
 	{ }						/* Terminating entry */
 };
-#endif
-
-static const struct usb_device_id usb_kbd_id_table[] = {
-	{
-		USB_INTERFACE_INFO(
-			USB_CLASS_MASS_STORAGE,
-			USB_SC_SCSI,
-			USB_PR_UAS
-			)
-	},
-	{ }						/* Terminating entry */
-};
-
-static int rurtles_usb_kbd_probe(struct usb_interface *interface,
-			const struct usb_device_id *id)
-{
-	pr_alert("[rurtle] Connected Sandisk 16GB USB stick");
-	return 0;
-}
-
-static void rurtles_usb_kbd_disconnect(struct usb_interface *intf)
-{
-	// pr_alert("Disconnected USB keyboard\n");
-	pr_alert("[rurtle] Disconnected Sandisk USB mass storage\n");
-}
 
 static struct usb_driver rurtles_usb_driver = {
-	.name = "rurtlesusbstick",
-	.probe = rurtles_usb_kbd_probe,
-	.disconnect = rurtles_usb_kbd_disconnect,
+	.name = "rurtlesusbwlkbd",
 	.id_table = usb_kbd_id_table,
 };
 
@@ -70,5 +43,5 @@ module_exit(rurtles_end);
 MODULE_DEVICE_TABLE(usb, usb_kbd_id_table);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("rurtle");
-MODULE_DESCRIPTION("Test module to check USB mass storage hotplugging");
+MODULE_DESCRIPTION("Test module for checking USB wireless keyboard hotplugging");
 MODULE_VERSION("0.01");
