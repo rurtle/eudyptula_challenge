a1975b45a608046a3b33d65702f4753dceaf62e8
Subhra Sarkar <rurtle.coder@gmail.com>
2020-12-24 15:58:52 +0530

    Added device definition and file operations - stubs

--- task_06/rurtles_misc_char.c
+++ task_06/rurtles_misc_char.c
@@ -7,6 +7,7 @@
 #include <linux/init.h>
 #include <linux/module.h>				/* Needed by all modules */
 #include <linux/kernel.h>
+#include <linux/fs.h>
 #include <linux/miscdevice.h>
 
 MODULE_LICENSE("GPL");
@@ -14,30 +15,42 @@ MODULE_AUTHOR("rurtle");
 MODULE_DESCRIPTION("Eudyptual Challenge - Task 06 - misc char driver");
 MODULE_VERSION("0.01");
 
-static struct miscdevice rurtles_misc_dev;
+static const struct file_operations rurtles_misc_fops = {
+	.owner			= THIS_MODULE,
+#if 0
+	.open			= rurtles_misc_open,
+	.read			= rurtles_misc_read,
+	.write			= rurtles_misc_write,
+	.close			= rurtles_misc_close,
+#endif
+};
 
-static int __init rurtles_start(void)
+static struct miscdevice rurtles_misc_dev = {
+	.name			= "eudyptula",
+	.minor			= MISC_DYNAMIC_MINOR,
+	.fops			= &rurtles_misc_fops,
+};
+
+static int __init rurtles_init(void)
 {
 	int retval;
 
-	rurtles_misc_dev.minor = MISC_DYNAMIC_MINOR;
-	rurtles_misc_dev.name = "eudyptula";
-	rurtles_misc_dev.fops = &rurtles_misc_fops;
 	retval = misc_register(&rurtles_misc_dev);
 
 	if (retval) {
-		pr_alert("[rurtle] Filed to register misc char driver");
+		pr_err("[rurtle] Filed to register misc char driver");
+		return retval;
 	}
-
+	/* Otherwise (misc device registered successfully) */
 	pr_debug("[rurtle] misc char driver 'eudyptula' registered!\n");
 	return 0;
 }
 
-static void __exit rurtles_end(void)
+static void __exit rurtles_exit(void)
 {
 	misc_deregister(&rurtles_misc_dev);
 	pr_alert("[rurtle] Goodbye World. misc char driver deregistered\n");
 }
 
-module_init(rurtles_start);
-module_exit(rurtles_end);
+module_init(rurtles_init);
+module_exit(rurtles_exit);
