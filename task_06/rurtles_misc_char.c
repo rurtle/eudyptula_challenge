69bea5c930d551a525a18443653ab25ba827b8e4
Subhra Sarkar <rurtle.coder@gmail.com>
2020-12-26 15:28:21 +0530

    write implementation

--- task_06/rurtles_misc_char.c
+++ task_06/rurtles_misc_char.c
@@ -15,8 +15,9 @@ MODULE_AUTHOR("rurtle");
 MODULE_DESCRIPTION("Eudyptual Challenge - Task 06 - misc char driver");
 MODULE_VERSION("0.01");
 
-#define	MY_ID		"XxXxXxXxXxXx"
+#define	MY_ID		"xxxxxxxxxxxx"
 #define	ID_LEN		sizeof(MY_ID)
+#define	TEXT_MSG	"eudyptula challenge - task 06 - write successful!"
 
 static int dev_opened;
 
@@ -42,14 +43,38 @@ static ssize_t rurtles_misc_read(struct file *fp, char __user *buf,
 	return simple_read_from_buffer(buf, nbytes, ppos, my_id, len);
 }
 
+static ssize_t rurtles_misc_write(struct file *fp,
+		const char __user *buf,
+		size_t nbytes, loff_t *ppos)
+{
+	int ret;
+	char my_id[ID_LEN];
+
+	memset(my_id, 0, sizeof(my_id));
+	ret = simple_write_to_buffer(my_id, sizeof(my_id) - 1, ppos,
+			buf, nbytes);
+	if (ret < 0) {
+		ret = -EFAULT;
+		pr_err("We don't know what went wrong.");
+	} else {
+		if (strncmp(my_id, MY_ID, strlen(my_id)) != 0) {
+			pr_err("Written string doesn't match ID.");
+			ret = -EINVAL;
+		} else {
+			pr_info("%s", TEXT_MSG);
+			ret = (ID_LEN - 1);
+		}
+	}
+
+	return ret;
+}
+
 static const struct file_operations rurtles_misc_fops = {
 	.owner			= THIS_MODULE,
 	.read			= rurtles_misc_read,
 	.open			= rurtles_misc_open,
 	.release		= rurtles_misc_release,
-#if 0
 	.write			= rurtles_misc_write,
-#endif
 };
 
 static struct miscdevice rurtles_misc_dev = {
@@ -65,11 +90,11 @@ static int __init rurtles_init(void)
 	retval = misc_register(&rurtles_misc_dev);
 
 	if (retval) {
-		pr_warn("[rurtle] Couldn't initialize miscdevice /dev/eudyptula.\n");
+		pr_warn("[rurtle] Couldn't initialize miscdevice /dev/eudyptula.");
 		return -ENODEV;
 	}
 
-	pr_info("[rurtle] Initialized device: /dev/eudyptula, node (MAJOR 10, MINOR %d).\n",
+	pr_info("[rurtle] Initialized device: /dev/eudyptula, node (MAJOR 10, MINOR %d).",
 		rurtles_misc_dev.minor);
 	return 0;
 }
@@ -77,7 +102,7 @@ static int __init rurtles_init(void)
 static void __exit rurtles_exit(void)
 {
 	misc_deregister(&rurtles_misc_dev);
-	pr_info("[rurtle] Unregistered miscdevice /dev/eudyptual.\n");
+	pr_info("[rurtle] Unregistered miscdevice /dev/eudyptual.");
 }
 
 module_init(rurtles_init);
